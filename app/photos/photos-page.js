0e1b4f0cc3b2355efc3b12c90132f55257be0e7d
gauravation <gauravation@gmail.com>
2018-10-25 17:29:06 +0530

    image upload

--- app/photos/photos-page.js
+++ app/photos/photos-page.js
@@ -11,10 +11,10 @@ const config = require("~/shared/config");
 //const platform = require("tns-core-modules/platform");
 const imageSourceModule = require("tns-core-modules/image-source");
 //const fileSystemModule = require("tns-core-modules/file-system");
-//const bghttp = require("nativescript-background-http");
-
-//const session = bghttp.session("file-upload");
+const bghttp = require("nativescript-background-http");
+const session = bghttp.session("file-upload");
 const fileSystemModule = require("tns-core-modules/file-system");
+var azure = require('fast-azure-storage')
 /* ***********************************************************
 * Use the "onNavigatingTo" handler to initialize the page binding context.
 *************************************************************/
@@ -104,70 +104,158 @@ function selectPhoto() {
 
 
 function selectPhoto2() {
-
-// //     //Generating Unique Image Name Using Time.
-// // vard=newDate();
-// // vart=d.getTime();
-// // this.picName="Upload"+t.toString()+".jpg";
+ 
+//     //Generating Unique Image Name Using Time.
+// vard=newDate();
+// vart=d.getTime();
+// this.picName="Upload"+t.toString()+".jpg";
   
-//     const mycontainer = config.AzureContainer;
-//     const azURL = "https://lumievents.blob.core.windows.net/" + mycontainer;
-//     const blobName = "sample2.jpg";
-//     const context = imagepicker.create({ mode: "single" }); // use "multiple" for multiple selection
-//     context
-//     .authorize()
-//     .then(() => {
-//         return context.present();
-//     })
-//     .then((selection) => {
-//         selection.forEach((selected) => {
 
-//             // Check file size is lower than 256MB
-//             // process the selected image
-//             // Create a blob
-//             let path = selected.android;
-//             let imageFromLocalFile = imageSourceModule.fromFile(path);
-//             let imageFile = fileSystemModule.File.fromPath(path);
-//             let binarySource = imageFile.readSync((err) => {
-//                 console.log(err);
-//             });
-//             let filesize = binarySource.size;
-//            let base64string = imageFromLocalFile.toBase64String("jpg");
-//             let constrequest =
-//                     {
-//                     url:azURL,
-//                     method:"PUT",
-//                     headers:
-//                     {
-//                     "cache-control":"no-cache",
-//                     "x-ms-blob-content-disposition":"attachment;",
-//                     "x-ms-meta-m2":"v2",
-//                     "x-ms-meta-m1":"v1",
-//                     "x-ms-blob-type":"BlockBlob",
-//                     "Content-Type":"application/octet-stream",
-//                     "File-Name":path,
-//                     "x-ms-date":"2018-10-24", //Provide date
-//                     "x-ms-version":"2018-03-28",
-//                     "Content-Length":filesize,
-//                     "Authorization": "SharedKey eventresources:gZJNy2J0LnZYKN+jhbs0ZjrFhCIRhX4/LBQtwv96+V0TO+x/OazEF7zQ3EWNnBYYi7oVYH1JdWNIwDZsoonw1A=="
-//                     },
-//                     description:{ "uploading": '+ path +"' },
-//                     };
 
-//                     const task = session.uploadFile(this.picture, request);
-//                     task.on("complete", (event) => {
-//                     console.log("Uploaded `"+ this.picture+"`");
-//                     });
 
-//                     task.on("error", event=> {
-//                     console.log(event);
-//                     console.log("Could not upload `"+ this.picture +"`. "+event.eventName);
-//                     });         
-//                             });
-//                         // list.items = selection;
-//                         }).catch((e) => {
-//                             // process error
-//                         });
+    const mycontainer = config.AzureContainer;
+    const azURL = "https://lumievents.blob.core.windows.net/" + mycontainer;
+    const blobName = "sample2.jpg";
+    const context = imagepicker.create({ mode: "single" }); // use "multiple" for multiple selection
+    context
+    .authorize()
+    .then(() => {
+        return context.present();
+    })
+    .then((selection) => {
+        selection.forEach((selected) => {
+
+            // Check file size is lower than 256MB
+            // process the selected image
+            // Create a blob
+
+          
+
+            let path = selected.android;
+            let imageFromLocalFile = imageSourceModule.fromFile(path);
+            let base64string = imageFromLocalFile.toBase64String('png');
+// Common options using shared key authentication
+var options = {
+    accountId:"eventresources",
+    accessKey:"gZJNy2J0LnZYKN+jhbs0ZjrFhCIRhX4/LBQtwv96+V0TO+x/OazEF7zQ3EWNnBYYi7oVYH1JdWNIwDZsoonw1A=="
+  };
+  var blob  = new azure.Blob(options);
+  var buffer = new Buffer(base64string, 'base64');
+  var blobContent = buffer;//'Sample content'; // The content can be a string or a Buffer
+// Create container and upload a blob
+blob.createContainer(mycontainer).then(function() {
+  return blob.putBlob(mycontainer, blobName, {
+    type:  'BlockBlob',     // Type of the blob 
+  }, blobContent);
+});
+
+
+
+
+
+
+
+            var blobService = azure.createBlobService();
+ 
+blobService.createBlockBlobFromLocalFile(mycontainer, blobName, path, function(error, result, response) {
+  if (!error) {
+    // file uploaded
+    
+  }
+  else {
+    alert(error);
+  }
+});
+            
+        //     let imageFromLocalFile = imageSourceModule.fromFile(path);
+        //     let imageFile = fileSystemModule.File.fromPath(path);
+            
+        //     let binarySource = imageFile.readSync((err) => {
+        //         console.log(err);
+        //     });
+
+        //     var stats = fileSystemModule.statSync(path);
+        //     var fileSizeInBytes = stats["size"];
+        //    // let filesize = imageFile.filesize; //binarySource.size;
+        //    let base64string = imageFromLocalFile.toBase64String("jpg");
+
+        //     let constrequest =
+        //             {
+        //             url:azURL,
+        //             method:"PUT",
+        //             headers:
+        //             {
+        //             "cache-control":"no-cache",
+        //             "x-ms-blob-content-disposition":"attachment;",
+        //             "x-ms-meta-m2":"v2",
+        //             "x-ms-meta-m1":"v1",
+        //             "x-ms-blob-type":"BlockBlob",
+        //             "Content-Type":"application/octet-stream",
+        //             "File-Name":"abhi.jpg",
+        //             "x-ms-date":"2018-10-24", //Provide date
+        //             "x-ms-version":"2018-03-28",
+        //             "Content-Length": ileSizeInBytes,
+        //             "Authorization": "SharedKey eventresources:gZJNy2J0LnZYKN+jhbs0ZjrFhCIRhX4/LBQtwv96+V0TO+x/OazEF7zQ3EWNnBYYi7oVYH1JdWNIwDZsoonw1A=="
+        //             },
+        //             description:{ "uploading": '+ path +"' },
+        //             };
+
+        //             const task = session.uploadFile(path, constrequest);
+        //             task.on("complete", (event) => {
+        //                 alert(path);
+        //             console.log("Uploaded `"+  path +"`");
+        //             });
+
+        //             task.on("error", event=> {
+        //             console.log(event);
+        //             console.log("Could not upload `"+ path +"`. "+event.eventName);
+        //             });         
+        //                     });
+                      
+                      
+                      
+                            // list.items = selection;
+                        }).catch((e) => {
+                            alert(e);
+                        });
+}
+
+function selectPhoto3() {
+    // var folder = fs.knownFolders.documents();
+    //         var pathOfImage = fs.path.join(folder.path, "Test.png");
+    //       // var saved = image.saveToFile(path,".png");
+    //        var request = 
+    //                {
+    //                    url : azURL,
+    //                        method : "POST",
+    //                        headers : {
+    //                    "Content-Type" : "application/octet-stream",
+    //                    "Authorization": "SharedKey eventresources:gZJNy2J0LnZYKN+jhbs0ZjrFhCIRhX4/LBQtwv96+V0TO+x/OazEF7zQ3EWNnBYYi7oVYH1JdWNIwDZsoonw1A=="
+    //                }
+    //            };
+    //        var task1 = session.uploadFile(path,request);
+
+    //        function onEvent(e) {
+    //         console.log("event: " + e.eventName);
+    //         if(e.object.description){
+    //           console.log(e.object.description);
+    //         }
+    //         if(e.error){
+    //           console.log(e.error);
+    //         }
+    //         if(e.currentBytes){
+    //           console.log(e.currentBytes + " of " + e.totalBytes);
+    //         }
+    //         if(e.data){
+    //           console.log(e.data);
+    //         }
+               
+    //         if(e.eventName == "complete"){
+    //              // mark blob as uploaded on QuickBlox side
+    //             alert('OK');
+    //         }
+    //     }
+    //        task1.on("complete", onEvent.bind(this));
 }
 
 exports.onNavigatingTo = onNavigatingTo;
